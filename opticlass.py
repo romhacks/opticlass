ba6b671a733cbe29b7eaf60e6351de59fd2dc534
Scarlett <scarlett@electronika.xyz>
2022-07-06 14:33:47 -0400

    add a bit more of the video backend

--- opticlass.py
+++ opticlass.py
@@ -20,7 +20,7 @@ window[0].update(1)
 # parse the command line
 parser = argparse.ArgumentParser(description="Classify objects in a video feed and get information about them", formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
 
-parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream (often /dev/video#, or a local file)")
+parser.add_argument("input_URI", type=str, default="/dev/video0", nargs='?', help="URI of the input stream (often /dev/video#, or a local file)")
 parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
 parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use")
 
@@ -37,8 +37,9 @@ window[0].update(2)
 net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 window[0].update(3)
 
-# create the video source
+# create the gstreamer video source and output
 input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
+output = jetson.utils.videoOutput("rtp://127.0.0.1:1234")
 window[0].update(4)
 
 # create the vlc instance and player
@@ -47,7 +48,9 @@ list_player = inst.media_list_player_new()
 media_list = inst.media_list_new([])
 list_player.set_media_list(media_list)
 player = list_player.get_media_player()
-player.set_xwindow(window['-VID_OUT-'].Widget.winfo_id())
+player.set_xwindow(window['-WEBCAM-'].Widget.winfo_id())
+media_list.add_media("rtp://127.0.0.1:1234")
+list_player.set_media_list(media_list)
 window[0].update(5)
 
 window.close()
@@ -59,3 +62,11 @@ layout = [
 ]
 
 window['-WEBCAM-'].expand(True, True) # resize video window to fill space
+
+while True:
+	# capture the next image
+    img = input.Capture()
+
+	# detect objects in the image (with overlay)
+    detections = net.Detect(img, overlay=opt.overlay)
+    output.Render(img)
