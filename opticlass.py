ddcb38d878d503c5a036c13d9925c7403a49c4ca
Scarlett <scarlett@electronika.xyz>
2022-07-07 14:10:36 -0400

    get the thing to work

--- opticlass.py
+++ opticlass.py
@@ -18,11 +18,15 @@ import jetson.utils # type: ignore
 window[0].update(1)
 
 # parse the command line
-parser = argparse.ArgumentParser(description="Classify objects in a video feed and get information about them", formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.", 
+                                 formatter_class=argparse.RawTextHelpFormatter, epilog=jetson.inference.detectNet.Usage() +
+                                 jetson.utils.videoSource.Usage() + jetson.utils.videoOutput.Usage() + jetson.utils.logUsage())
 
-parser.add_argument("input_URI", type=str, default="/dev/video0", nargs='?', help="URI of the input stream (often /dev/video#, or a local file)")
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
 parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
-parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
 
 try:
         opt = parser.parse_known_args()[0]
@@ -31,38 +35,43 @@ except:
         parser.print_help()
         sys.exit(0)
 
+is_headless = ["--headless"] if sys.argv[0].find('console.py') != -1 else [""]
+
 window[0].update(2)
 
-# load the specified network
+output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv+is_headless)
+	
+# load the object detection network
 net = jetson.inference.detectNet(opt.network, sys.argv, opt.threshold)
 window[0].update(3)
 
-# create the gstreamer video source and output
+# create video sources
 input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
-output = jetson.utils.videoOutput("rtp://127.0.0.1:1234")
 window[0].update(4)
 
 # create the vlc instance and player
+'''
 inst = vlc.Instance()
 list_player = inst.media_list_player_new()
 media_list = inst.media_list_new([])
 list_player.set_media_list(media_list)
 player = list_player.get_media_player()
-
+'''
 # setup the main window
 layout = [
     [gui.Text("OptiClass")],
     [gui.Image('', size=(300, 170), key='-WEBCAM-')],
 ]
+'''
 media_list.add_media("rtp://127.0.0.1:1234")
 list_player.set_media_list(media_list)
 window[0].update(5)
-
+'''
 window.close()
 
 window = gui.Window("OptiClass GUI", layout, finalize=True)
 window['-WEBCAM-'].expand(True, True) # resize video window to fill space
-player.set_xwindow(window['-WEBCAM-'].Widget.winfo_id()) 
+"player.set_xwindow(window['-WEBCAM-'].Widget.winfo_id())"
 
 while True:
 	# capture the next image
@@ -70,4 +79,4 @@ while True:
 
 	# detect objects in the image (with overlay)
     detections = net.Detect(img, overlay=opt.overlay)
-    output.Render(img)
+    output.Render(img)  
